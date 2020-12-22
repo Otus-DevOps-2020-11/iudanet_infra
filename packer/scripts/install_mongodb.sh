26ef1aba22289b85113f13ade47aed09a3f1e0c8
Aleksandr Chudakov <chudo88@gmail.com>
2020-12-22 22:48:50 +0300

    init hm-6

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -5,7 +5,7 @@ DEBIAN_FRONTEND=noninteractive
 sleep 10
 
 apt-get update
-apt-get install -y apt-transport-https ca-certificates gnupg
+apt-get install -y apt-transport-https ca-certificates gnupg libssl-dev
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
