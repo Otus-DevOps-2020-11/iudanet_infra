be0b9165ad3eb09888bd25ad0139648e3109f567
Aleksandr Chudakov <chudo88@gmail.com>
2020-12-22 23:44:53 +0300

    add lb

---
+++ terraform/lb.tf
@@ -0,0 +1,31 @@
+resource "yandex_lb_target_group" "app-target-group" {
+  name = "app-target-group-id"
+  target {
+    subnet_id = var.subnet_id
+    address   = yandex_compute_instance.app.network_interface.0.ip_address
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "app-balanser" {
+  name = "app-load-balancer"
+
+  listener {
+    name        = "my-listener"
+    port        = 80
+    target_port = 9292
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_lb_target_group.app-target-group.id
+
+    healthcheck {
+      name = "tcp"
+      tcp_options {
+        port = 9292
+      }
+    }
+  }
+}
