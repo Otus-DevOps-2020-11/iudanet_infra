af3e6dc0c21c1e0c4555bba20763d5659386a73a
Aleksandr Chudakov <chudo88@gmail.com>
2020-12-26 01:51:07 +0300

    add backernd terraform

---
+++ terraform/stage/backend.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "otusdevops2020-tf-backend"
+    region   = "us-east-1"
+    key      = "1/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "otusdevops2020-tf-backend"
+    region   = "us-east-1"
+    key      = "1/terraform.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
